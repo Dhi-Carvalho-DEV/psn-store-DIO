d9dca990258523c9d77a2aa4974ce459b1257ed9
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-10-20 08:53:58 -0300

    Repassando as propriedades do card para a página home

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -3,17 +3,16 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.css']
+  styleUrls: ['./card-pricing.component.css'],
 })
 export class CardPricingComponent implements OnInit {
-
   @Input()
-  gameType: string = ""
+  gameInfo: String = '';
   @Input()
-  gamePrice: string = ""
-  constructor() { }
+  gameType: string = '';
+  @Input()
+  gamePrice: string = '';
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
